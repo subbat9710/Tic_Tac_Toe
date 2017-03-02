12c07d89e1afbdcf99f6b04ef8a9cac687b43ddc
subbat9710 <tula.subba1991@gmail.com>
2017-03-01 21:04:25 -0500

    Test for Board.

--- board.rb
+++ board.rb
@@ -1,10 +1,22 @@
 class Board
 	attr_accessor :board 
 
-    def initialize()
+    def initialize
         @board = ["", "", "", "", "","", "", "", ""]
     end
-    def setposition(position, marker)
+
+    def set_position(position, marker)
        @board[position] = marker
+    end
+
+    def check_position?(position)
+    	if @board[position] == ""
+    		true
+    	else
+    		false
+    	end
+    end
+    def check_full?()
+    	@board.count("") == 0
     end
 end
--- testboard.rb
+++ testboard.rb
@@ -10,22 +10,92 @@ class TestBoard < Minitest::Test
 	end
 	def test_2_update_first_position_with_x
 		board = Board.new
-		board.setposition(0, "x")
+		board.set_position(0, "x")
 		result =["x", "", "", "", "","", "", "", ""]
 		assert_equal(result, board.board)
     end
     def test_for_o_for_the_fifth_position
 		board = Board.new
-		board.setposition(0, "x")
-		board.setposition(5, "o")
+		board.set_position(0, "x")
+		board.set_position(5, "o")
 		result = ["x", "", "", "", "","o", "", "", ""]
 		assert_equal(result, board.board)
     end
+    def test_for_o_for_the_seventh_position
+		board = Board.new
+		board.set_position(0, "x")
+		board.set_position(7, "o")
+		result = ["x", "", "", "", "","", "", "o", ""]
+		assert_equal(result, board.board)
+    end
+    def test_for_x_for_the_eight_position
+		board = Board.new
+        board.board = ["x", "", "", "", "","o", "", "", ""]
+		board.set_position(8, "x")
+        result = ["x", "", "", "", "","o", "", "", "x"]
+		assert_equal(result, board.board)
+    end
+    def test_for_x_for_the_second_position
+		board = Board.new
+        board.board = ["x", "", "", "", "","o", "", "", ""]
+		board.set_position(2, "x")
+        result = ["x", "", "x", "", "","o", "", "", ""]
+		assert_equal(result, board.board)
+    end
+    def test_for_0_for_7_position
+		board = Board.new
+        board.board = ["x", "", "", "", "","o", "", "", ""]
+		board.set_position(7, "x")
+        result = ["x", "", "", "", "","o", "", "x", ""]
+		assert_equal(result, board.board)
+    end
+    def test_for_0_for_6_position
+		board = Board.new
+        board.board = ["x", "", "", "", "","o", "", "", ""]
+		board.set_position(6, "x")
+        result = ["x", "", "", "", "","o", "x", "", ""]
+		assert_equal(result, board.board)
+	end
     def test_for_o_for_the_eight_position
 		board = Board.new
         board.board = ["x", "", "", "", "","o", "", "", ""]
-		board.setposition(8, "x")
-        result = ["x", "", "", "", "","o", "", "", "x"]
-		assert_equal(result, board.board)
+        result = board.check_position?(0)
+		assert_equal(false, result)
+    end
+    def test_for_o_for_the_0_position_in_6
+		board = Board.new
+        board.board = ["x", "0", "", "", "","o", "", "", ""]
+		result = board.check_position?(1)
+		assert_equal(false, result)
+    end
+    def test_for_minus_four_position
+    	board = Board.new
+    	board.board = ["x", "0", "", "", "", "", "x", "x", "x"]
+    	result = board.check_position?(-4)
+    	assert_equal(true, result)
+    end
+    def test_for_fiften_position
+    	board = Board.new
+    	board.board = ["x", "0", "", "", "", "", "x", "x", "x"]
+    	result = board.check_position?(15)
+    	assert_equal(false, result)
+    end
+    def test_full_board_equals_game_over
+    	board = Board.new
+    	board.board = ["x", "0", "0", "0", "0", "x", "x", "x", "x"]
+    	result = board.check_full?
+        assert_equal(true, result)
+    end
+    def test_almost_full
+    	board = Board.new
+    	board.board = ["x", "x", "0", "0", "0", "", "x", "x", "x"]
+    	result = board.check_full?
+    	assert_equal(false, result)
+    end
+    def test_empty_board
+    	board = Board.new
+    	board.board = ["", "", "", "", "", "", "", "", ""]
+    	result = board.check_full?
+    	assert_equal(false, result)
     end
 end
