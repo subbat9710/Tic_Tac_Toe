a69a5fb474f40631cfac14572877f02f343c441a
subbat9710 <tula.subba1991@gmail.com>
2017-05-20 22:52:05 -0400

    refactoring the board for the test

--- board.rb
+++ board.rb
@@ -1,4 +1,4 @@
-class Board
+class Board 
 	attr_accessor :board, :winner_of_game #attr_accessor turn instance variable into instance method ( like @ no longer required ) in the class definition.
 
     def initialize
